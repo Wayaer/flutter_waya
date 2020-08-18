542a3e826fbafd8f3a2c700a23c6c04236c9afdd
Wayaer <wayaer@foxmail.com>
2020-08-18 11:42:34 +0800

    no message

--- lib/src/widget/custom/Universal.dart
+++ lib/src/widget/custom/Universal.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_waya/flutter_waya.dart';
 
 class Universal extends StatelessWidget {
   final EdgeInsetsGeometry padding;
