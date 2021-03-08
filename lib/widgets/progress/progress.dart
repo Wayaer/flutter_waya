f977beec20061bf2f553efae2ceda11a0a8d239e
Wayaer <wayaer@foxmail.com>
2021-03-08 12:49:23 +0800

    no message

--- lib/widgets/progress/progress.dart
+++ lib/widgets/progress/progress.dart
@@ -2,7 +2,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_waya/flutter_waya.dart';
-import 'package:vector_math/vector_math.dart';
 
 class Progress extends StatefulWidget {
   Progress.circular({
