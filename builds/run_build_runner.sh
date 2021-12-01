315704808d363d3ad196e176022d744c1f5bf7d6
Wayaer <wayaer@foxmail.com>
2021-12-01 17:13:05 +0800

    add some

--- builds/run_build_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-file=".dart_tool/package_config.json"
-
-if [[ ! -f "$file" ]]; then
-  flutter pub get
-fi
-
-flutter packages pub run build_runner build --delete-conflicting-outputs
