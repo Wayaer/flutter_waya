0c0b17b3d55998f9a31179e8156893fd81248c67
wayaer <wayaer@foxmail.com>
2022-10-14 22:08:48 +0800

    no message

--- example/app/web/flutter_service_worker.js
+++ example/app/web/flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "11e1a177d57c5da2bc6e3a6acc3010f4",
-"index.html": "13d16b089603289052ad053e466802ee",
-"/": "13d16b089603289052ad053e466802ee",
-"main.dart.js": "383822cadeb37806a70333b69212d946",
+"index.html": "c318d7734086a2e9904e4e9faae2a64d",
+"/": "c318d7734086a2e9904e4e9faae2a64d",
+"main.dart.js": "81c2e77c6d6ffb583b920bf3ec4df3ca",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "7b1e138b59ab213ab88128833a2681f3",
 "icons/Icon-192.png": "786a784711f7ba2dcc1aa8f4fa52d02b",
 "icons/Icon-512.png": "8a74e06ad551887241db3b8c87b78697",
@@ -15,8 +16,12 @@ const RESOURCES = {
 "assets/NOTICES": "a54dd4e7b711fa10540c37910cd43bd6",
 "assets/FontManifest.json": "6ebd589d0bb9e5df6f3206c62ba58999",
 "assets/packages/flutter_waya/lib/res/icons.ttf": "a02e17b77a95bf0bc05d8d786feb1613",
-"assets/shaders/ink_sparkle.frag": "b2f01e0742ad83f4f16ceb93ac804f62",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b"
+"assets/shaders/ink_sparkle.frag": "3d213db0d4722b75edbd916ea54beedf",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
 };
 
 // The application shell files that are downloaded before a service worker can
