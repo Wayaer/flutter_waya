64f79f554609ec8c4fa2800337f7f27afdf216c0
Wayaer <wayaer@foxmail.com>
2023-09-19 16:50:43 +0800

    no message

--- example/app/web/flutter_service_worker.js
+++ example/app/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "11e1a177d57c5da2bc6e3a6acc3010f4",
-"index.html": "af92e5cc732590c2e7ebfdb4de22c5d6",
-"/": "af92e5cc732590c2e7ebfdb4de22c5d6",
-"main.dart.js": "9c1f174c07404a38b8b3cbae86572147",
+"index.html": "c8cc8214ed016b3206d35af4fa8b9a24",
+"/": "c8cc8214ed016b3206d35af4fa8b9a24",
+"main.dart.js": "f377f6c6888353c085b7d71bdf6c5d73",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -23,10 +23,10 @@ const RESOURCES = {"version.json": "11e1a177d57c5da2bc6e3a6acc3010f4",
 "assets/fonts/MaterialIcons-Regular.otf": "73468375551012db80c0710e2cad3c05",
 "canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
 "canvaskit/skwasm.wasm": "d1fde2560be92c0b07ad9cf9acb10d05",
-"canvaskit/chromium/canvaskit.js": "ffb2bb6484d5689d91f393b60664d530",
-"canvaskit/chromium/canvaskit.wasm": "393ec8fb05d94036734f8104fa550a67",
-"canvaskit/canvaskit.js": "5caccb235fad20e9b72ea6da5a0094e6",
-"canvaskit/canvaskit.wasm": "d9f69e0f428f695dc3d66b3a83a4aa8e",
+"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee22aebb0",
+"canvaskit/chromium/canvaskit.wasm": "1165572f59d51e963a5bf9bdda61e39b",
+"canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
+"canvaskit/canvaskit.wasm": "19d8b35640d13140fe4e6f3b8d450f04",
 "canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15"};
 // The application shell files that are downloaded before a service worker can
 // start.
--- example/build_web.sh
+++ example/build_web.sh
@@ -6,6 +6,6 @@ flutter packages get
 
 echo "开始构建 web"
 
-flutter build web --web-renderer html
+flutter build web --web-renderer canvaskit
 
 mv 'build/web' 'app/web'
